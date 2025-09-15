bf67913a67ea9f6e29bd6d4cd37ede12b863b9d7
BurtonNeko <chenxili229@gmail.com>
2025-09-15 11:04:23 +0800

    Create task_6.py

---
+++ task_6.py
@@ -0,0 +1,14 @@
+# Week 2, Session 1: Task 6
+
+from pprint import pprint
+
+# Create music database, as a dictionary of strings mapped to lists
+music_db = {
+    "King Crimson": ["Red", "In the Court of the Crimson King"],
+    "Pink Floyd": ["The Division Bell", "The Dark Side of the Moon", "The Wall"],
+    "Joy Division": ["Unknown Pleasures", "Heart and Soul"] 
+}
+print("My Favorate Music")
+pprint(music_db)
+
+# Pretty-print the data structure
