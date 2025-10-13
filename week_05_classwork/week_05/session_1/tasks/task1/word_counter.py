6a5e44833eea8eee25904db65bca939ac28e0618
BurtonNeko <chenxili229@gmail.com>
2025-10-13 10:21:25 +0800

    Update word_counter.py

--- week_05_classwork/week_05/session_1/tasks/task1/word_counter.py
+++ week_05_classwork/week_05/session_1/tasks/task1/word_counter.py
@@ -7,10 +7,20 @@
 # You can use any of the .txt files to test this - or even make your own.
 
 # get the file name
-
+def count_words_in_file():
+  
+    filename = input('The text to be read: ')
 # open and read the file
+    try:        
+          with open(filename, 'r', encoding='utf-8') as file:
+              total_words = 0
+              for line in file:
+                  words = [word for word in line.strip().split() if word]
+                  total_words += len(words)
+          print(f"Text '{filename}' has total words of {total_worlds}")   
 # hint: remember to handle the case where the file might not exist
-
+    except FileNotFoundError:
+          print(f'Text Not Found')
 # calculate the number of words
 # hint: you could use .split() to break up by spaces
 # hint: watch out for empty lines that might give you empty strings
