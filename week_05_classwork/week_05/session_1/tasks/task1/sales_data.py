48a8e30423aa242b5664b7090331f7ca029b7dd7
BurtonNeko <chenxili229@gmail.com>
2025-10-13 09:31:07 +0800

    Update sales_data.py

--- week_05_classwork/week_05/session_1/tasks/task1/sales_data.py
+++ week_05_classwork/week_05/session_1/tasks/task1/sales_data.py
@@ -1,9 +1,44 @@
 # You've been asked to analyse a set of sales data which you can find in sales.csv
-
+def read_sales_filedata(fliename):
+  try:
+    with open(filename, 'r', encoding = 'utf - 8')
+    lines = file.readlines()[1:]
+    data = [line.strip().split(',') for line in lines if len(line.strip().split(',')) == 5]
+    return data
+  except FileNotFoundError:
+    print('File Not Found')
+    return[]  
 # Open the file and read the data
 # find the:
 #   - largest sale day (highest sale amount)
+  def find_lagest_saleday(data):
+    max_sale = 0
+    max_row = None
+    for row in data:
+      date, product, sales_amount, units_sold, region
+      try:
+        amount = int(sale_amount:)
+        if amount > max_sale
+        max_sale = amount
+        max_row = row
+      except ValueError:
+        continue
+      if max_row:
+        print{f'Lagest sale was on {max_row[0]}'}
 #   - average sale amount
+    def find_average_sale_amount(data):
+      total = 0
+      count = 0
+      for row in data:
+        try:
+          total += int(row[2])
+          count += 1
+        except ValueError:
+          continue
+      if count > 0:
+        average = total / count
+        
+          
 #   - the widget which has been sold the most
 # and print these out in a nice, human-readable format
 
