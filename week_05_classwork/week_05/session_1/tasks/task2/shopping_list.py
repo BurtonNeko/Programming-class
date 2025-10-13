650a67676fc66893094cfbc8759d90389dcfe3b6
BurtonNeko <chenxili229@gmail.com>
2025-10-13 11:20:22 +0800

    Update shopping_list.py

--- week_05_classwork/week_05/session_1/tasks/task2/shopping_list.py
+++ week_05_classwork/week_05/session_1/tasks/task2/shopping_list.py
@@ -1,10 +1,19 @@
 # Step 1: Create an empty list for the shopping list.
 # optional: see if there is an existing shopping list file and load that
 # hint: use try/except to check if the file exists, or import os and use os.path.exists()
-
+import os
+shopping_list = []
+def load_existing_list(filename = 'shopping_list.txt'):
+  if os.path.exists(filename):
+    try:
+      with open(filename, 'r', encoding = 'utf-8') as f:
+        for line in f:
+          item = line.strip()
+          if item:
+              shopping_list.append(item)
+      print('Laod existing shopping list.')
 # Step 2: Define a function to add an item to the list.
 # Prompt the user for the item name and add it to the list.
-
 # Step 3: Define a function to remove an item from the list.
 # Prompt the user for the item name to remove and delete it from the list if it exists.
 # hint: use list.remove() or check if item is in list first
